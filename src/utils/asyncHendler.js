166ea5bcbbd7eff50babf488f75623d033c98add
pankajkumarsrm <adityagupta4962@gmail.com>
2024-05-03 15:51:08 +0530

    utils discussiion

---
+++ src/utils/asyncHendler.js
@@ -0,0 +1,28 @@
+/* -- use promisse in create function -- */
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/*---- diffrent diffrent type use function ----- */
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () =>{}
+
+// use try catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
