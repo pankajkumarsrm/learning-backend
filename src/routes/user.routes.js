bc594b1d9ae6d380806934240950f9f91d722201
pankajkumarsrm <adityagupta4962@gmail.com>
2024-05-05 13:53:57 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+  upload.fields([
+    //fields multilple file store kerta hai  // images upload cloudinary
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 //router.route("/login").post(login)
-
-
 
 export default router;
